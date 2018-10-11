18d1383bef1dab8b933063ce4639bfa0b40b9755
ttbf <juquansheng@hotmail.com>
2018-10-11 14:03:41 +0800

    jenkins

--- src/main/java/com/yicuojin/football/Scheduling/MatchCmoponent.java
+++ src/main/java/com/yicuojin/football/Scheduling/MatchCmoponent.java
@@ -35,13 +35,13 @@ public class MatchCmoponent {
     /**
      * Description: (定时器 每五分钟)竞猜是否结束
      */
-    @Scheduled(cron = "0 0/5 *  * * ? ")
+   /* @Scheduled(cron = "0 0/5 *  * * ? ")
     public void matchStart() {
 
         logger.info("调起定时任务，比赛是否结束竞猜....");
         boolean b = matchService.matechResult();
         System.out.println(b);
 
-    }
+    }*/
 
 }
